8dc77fbca5a36d0f1a53bb0a77239485733035a0
captaincrackerjack <gdnunley@outlook.com>
2023-05-16 02:45:52 -0500

    added responsiveness to Project page

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -11,23 +11,26 @@ export default async function Blog() {
       <h1 className="text-4xl drop-shadow font-extrabold text-center text-gray-200">
         {page.title}
       </h1>
-      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="mt-20 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {posts.map((post) => (
           <Link
             href={`/blog/${post.slug}`}
             key={post._id}
-            className="border-2 border-gray-500 rounded-lg p-3 hover:scale-105 hover:border-blue-500 transition flex flex-col items-center"
+            className="border-gray-500 rounded-lg p-4 hover:scale-105 hover:border-blue-500 transition flex flex-col items-center"
           >
-            <div className="relative w-full pb-full"></div>
-            {post.mainImage && (
-              <Image
-                src={post.mainImage}
-                alt={post.title}
-                width={750}
-                height={300}
-                className="object-cover rounded-lg border border-gray-500"
-              />
-            )}
+            <div className="relative w-64 h-64">
+              {" "}
+              {/* Set a fixed size for the card */}
+              {post.mainImage && (
+                <Image
+                  src={post.mainImage}
+                  alt={post.title}
+                  width={750} // You can adjust these values to suit your needs
+                  height={300} // You can adjust these values to suit your needs
+                  className="absolute top-0 left-0 w-full h-full object-cover rounded-lg border border-gray-500"
+                />
+              )}
+            </div>
             <div className="mt-2 font-extrabold bg-gradient-to-r via-red-500 from-orange-400 to-purple-600 bg-clip-text text-transparent text-center w-full">
               {post.title}
             </div>
--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -11,36 +11,37 @@ export default async function Project({ params }: Props) {
   const project = await getProject(slug);
 
   return (
-    <div>
-      <header className="flex items-center justify-between">
-        <h1 className="text-4xl drop-shadow font-extrabold text-center text-gray-200">
+    <div className="flex flex-col justify-center space-y-12 items-center">
+      <div className="w-3/4 md:w-1/2 lg:w-1/3 text-center">
+        <h1 className="text-4xl drop-shadow font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-gray-200 to-gray-200 hover:from-pink-500 hover:to-orange-500 transition-all duration-500 ease-in-out transform hover:scale-110">
           {project.name}
         </h1>
-
+      </div>
+      <div className="flex justify-center w-full md:w-2/3 lg:w-3/4">
+        <Image
+          src={project.image}
+          alt={project.name}
+          width={1000} // Adjust these values to suit your needs
+          height={800} // Adjust these values to suit your needs
+          className="border-2 border-gray-700 object-cover rounded-xl"
+        />
+      </div>
+      <div className="w-3/4 md:w-1/2 lg:w-1/3 text-center">
         <a
           href={project.url}
           title="View project"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition"
+          className="inline-block bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-blue-500 hover:text-white transition transform hover:scale-110"
         >
           View Project
         </a>
-      </header>
-
-      {/* content goes here */}
-      <div className="text-lg text-gray-700 mt-5">
-        <PortableText value={project.content} />
       </div>
-
-      {/* image goes here */}
-      <Image
-        src={project.image}
-        alt={project.name}
-        width={1920}
-        height={1080}
-        className="mt-10 border-2 border-gray-700 object-cover rounded-xl"
-      />
+      <div className="w-full md:w-3/4 lg:w-2/3 text-left">
+        <div className="text-lg text-gray-400">
+          <PortableText value={project.content} />
+        </div>
+      </div>
     </div>
   );
 }
